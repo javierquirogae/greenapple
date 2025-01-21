9c80ff72979b20f22fbc1967c01e77279c7000f7
Jacob Quiroga <jacobquiroga01@gmail.com>
2025-01-20 20:16:32 -0500

    styles

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
+import styles from './src/assets/output.css'
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Greenapple logo" class="logo" src="@/assets/GA_LOGO.png" width="125" height="125" />
-
+    <div class="bg-indigo-300">
+      <img alt="Greenapple logo" class="w-full" src="@/assets/GA_LOGO.png" />
+    </div>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
