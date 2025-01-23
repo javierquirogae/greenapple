c5b94e4141e883ccec2817097ca64954506725ff
Jacob Quiroga <jacobquiroga01@gmail.com>
2025-01-22 22:13:18 -0500

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 // vue.config.js file to be placed in the root of your repository
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/greenapple/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/greenapple/' : '/dist',
 }
